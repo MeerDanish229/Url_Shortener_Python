1da75f562109e4347958b128908aa16fff49e527
MeerDanish229 <128418303+MeerDanish229@users.noreply.github.com>
2024-07-07 15:59:26 +0500

    Update urlShortener.py

--- urlShortener.py
+++ urlShortener.py
@@ -1,27 +1,27 @@
 import pyshorteners
 import tkinter as tk
-from tkinter import *
 
-def generate_short_url(parm):
-    parm = url_entry.get()
-    # short_url_entry.config(state='normal',bg='light blue')
-    if parm == "":
-         short_url_entry.delete(0, END) 
-         short_url_entry.insert(END,"Please Enter Url")
-         short_url_entry.config(bg='light blue')     
-         short_url_entry.config(state='readonly') 
+def generate_short_url():
+    url = url_entry.get()
+    if url == "":
+        short_url_label.config(text="Please Enter Url", bg='light pink')
     else:
-         short_url = pyshorteners.Shortener().tinyurl.short(parm)
-        #  short_url_entry.config(state='normal')    
-         short_url_entry.delete(0, END) 
-         short_url_entry.insert(END,short_url)
-         url_entry.delete(0,END)
-        #  short_url_entry.config(bg='light pink') 
-    # short_url_entry.config(state='readonly')
-         
-    # short_url = pyshorteners.Shortener().tinyurl.short(parm)
-    # short_url_entry.delete(0, END) 
-    # short_url_entry.insert(END,short_url)
+        try:
+            url_entry.delete(0, tk.END)
+            short_url = pyshorteners.Shortener().tinyurl.short(url)
+            short_url_label.config(text=short_url, bg='light blue')
+        except Exception as e:
+            short_url_label.config(text="Error: Invalid URL", bg='light pink')
+
+def copy_to_clipboard():
+    short_url = short_url_label.cget("text")
+    if short_url and "http" in short_url: 
+        root.clipboard_clear()
+        root.clipboard_append(short_url)
+        root.update()  
+        copy_label.config(text="Copied!", bg='light green')
+    else:
+        copy_label.config(text="Nothing to copy", bg='light pink')            
 
 def output_window_center(parm):
     parm.update_idletasks()
@@ -29,21 +29,29 @@ def output_window_center(parm):
     height = parm.winfo_height()
     x = (parm.winfo_screenwidth() // 2) - (width // 2)
     y = (parm.winfo_screenheight() // 2) - (height //2)
-    parm.geometry("{}x{}+{}+{}".format(width,height,x,y))
-
+    parm.geometry("{}x{}+{}+{}".format(width, height, x, y))
 
 root = tk.Tk()
-root.title("Codexcue Url Shortner")
+root.title("Codexcue Url Shortener")
 root.configure(bg="light blue")
 root.geometry('600x500')
-root.resizable(False,False)
+root.resizable(False, False)
 output_window_center(root)
 
-tk.Label(root,text="Enter Url :",font=('impact',20,'italic'),bg='light blue').pack(pady=10)
-url_entry = tk.Entry(root,width=100,font=('Ariel 18 bold'))
+tk.Label(root, text="Enter Url:", font=('impact', 20, 'italic'), bg='light blue').pack(pady=10)
+url_entry = tk.Entry(root, width=100, font=('Ariel 18 bold'))
 url_entry.pack(padx=10)
-url_btn =tk.Button(root,text="Generate Short Url" ,font=('Sans 16 bold'),command=lambda:generate_short_url(url_entry)).pack(pady=5)
-short_url_entry = tk.Entry(root, width=80, font=('Ariel 16 italic'), bd=0, bg='light blue', foreground='black')
-short_url_entry.pack(pady=20, padx=20)
-# short_url_entry.config(background='light blue',state='readonly')
+
+url_btn = tk.Button(root, text="Generate Short Url", font=('Sans 16 bold'), command=generate_short_url)
+url_btn.pack(pady=5)
+
+short_url_label = tk.Label(root, text="", font=('Ariel 16 italic'), bg='light blue', foreground='black', wraplength=500)
+short_url_label.pack(pady=20, padx=20)
+
+copy_button = tk.Button(root, text="Copy URL", command=copy_to_clipboard)
+copy_button.pack(pady=5)
+
+copy_label = tk.Label(root, text="", width=50, height=2, bg='light grey')
+copy_label.pack(pady=10)
+
 root.mainloop()
